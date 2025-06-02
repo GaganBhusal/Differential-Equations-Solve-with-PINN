106166aaa8188f2e577b7c8c7fcece0e062ca200
Gagan Bhusal <gaganbhusal20@gmail.com>
2025-06-02 23:22:45 +0545

    Added Interactive 3D Plot

--- HeatEquation1D.py
+++ HeatEquation1D.py
@@ -1,117 +1,144 @@
-import streamlit as st
+import plotly.graph_objects as go
 import numpy as np
+import sympy as sp
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
-import matplotlib.pyplot as plt
 
-
-class StreamlitPage():
+class NN(nn.Module):
 
     def __init__(self):
-        super(StreamlitPage, self).__init__()
-        self.value_entered = False
+        super(NN, self).__init__()
+        self.seq = nn.Sequential(
+            nn.Linear(2, 64),
+            nn.Tanh(),
+            nn.Linear(64, 64),
+            nn.Tanh(),
+            nn.Linear(64, 1),
+            nn.Tanh()
+        )
 
+    def forward(self, x, t):
 
-    def buildPage(self):
-        self.create_left_sidebar()
-        self.show_from_sidebar()
+        data = torch.cat([x, t], dim = 1)
+        return self.seq(data)
 
+class PINN:
 
-    def create_left_sidebar(self):
-        self.left_sidebar = st.sidebar.selectbox(
-                    "Equations",
-                    ("1D Heat Equation", "1D Wave Equation"),
-                    index = None,
-                    placeholder = "Choose an equation"
-                )
+    def __init__(self, x, t, ic_func, bc0, bc1):
+
+        self.x_initial = x
+        self.t_initial = t
+
+        self.linear_points = 1000
+
+        symbols = sp.symbols('x')
+        expr = sp.sympify(ic_func)
+        self.ic0 = sp.lambdify(symbols, expr, modules=['numpy'])
+
+        self.bc0 = bc0
+        self.bc1 = bc1
+
+        self.alpha = 0.1
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         
-    def show_from_sidebar(self):
-        if self.left_sidebar == "1D Heat Equation":
-            self.HeatEquation1D()
-        elif self.left_sidebar == "1D Wave Equation":
-            self.WaveEquation1D()
+        self.model = NN()
+        self.model.to(self.device)
+        self.generate_values()
         
-    def HeatEquation1D(self):
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr = 0.001)
 
-        with st.form("Main"):
+    def generate_values(self):
+        
+        self.x_ic = torch.linspace(0, self.x_initial, self.linear_points).view(-1, 1).to(self.device)
+        self.t_ic = torch.zeros_like(self.x_ic).to(self.device)
+        self.u_ic = torch.sin(torch.pi * self.x_ic).to(self.device)
+        self.u_ic = torch.tensor(self.ic0(self.x_ic)).to(self.device)
 
-            st.write("1D Heat Equation")
-            left_column, right_column = st.columns(2)
-            with left_column:
-                st.markdown("**Length of rod and time**")
-                st.write("")
-                lcl, lcr = st.columns([1, 1])
-                with lcl:
-                    st.markdown("**Length of rod**")
-                    st.write("")
-                    st.markdown("**Total time**")
-                with lcr:
-                    self.x = st.number_input(
-                        "", 
-                        value = 1, 
-                        placeholder = 1.0,
-                        key = "x",
-                        label_visibility = "collapsed"
-                    )
-                    # st.write("")
-                    self.t = st.number_input(
-                        "", 
-                        value = 1, 
-                        placeholder = 1.0,
-                        key = "t",
-                        label_visibility = "collapsed"
-                    )
-            with right_column:
+        self.t_bc = torch.linspace(0, self.t_initial, self.linear_points).view(-1, 1).to(self.device)
+        self.x_bc0 = torch.zeros_like(self.t_bc).to(self.device)
+        self.u_bc0 = torch.zeros_like(self.t_bc).to(self.device)
+        self.x_bc1 = torch.ones_like(self.t_bc).to(self.device)
+        self.u_bc1 = torch.zeros_like(self.t_bc).to(self.device)
+
+        self.x = torch.rand(1000).view(-1, 1).to(self.device)
+        self.x.requires_grad = True
+        self.t = torch.rand(1000).view(-1, 1).to(self.device)
+        self.t.requires_grad = True
+
+
+    
+    def calc_pde_loss(self):
+
+        u = self.model(self.x, self.t)
+
+        u_x = torch.autograd.grad(u, self.x, grad_outputs=torch.ones_like(u), create_graph=True)[0]
+        u_t = torch.autograd.grad(u, self.t, grad_outputs=torch.ones_like(u), create_graph=True)[0]
+        u_xx = torch.autograd.grad(u_x, self.x, grad_outputs=torch.ones_like(u_x), create_graph=True)[0]
+
+        return torch.mean((u_t - self.alpha * u_xx) ** 2)
+
+
+    def train(self, epochs = 1000):
+        self.model.train()
+        self.epoch_loss = []
+        self.ic_loss = []
+        self.bc_loss = []
+        self.pde_loss = []
+        for epoch in range(epochs):
+            self.optimizer.zero_grad()
+
+            u_ic_pred = self.model(self.x_ic, self.t_ic)
+            loss_ic = torch.mean((self.u_ic - u_ic_pred) ** 2)
+
+            u_bc0_pred = self.model(self.x_bc0, self.t_bc)
+            u_bc1_pred = self.model(self.x_bc1, self.t_bc)
+            loss_bc = torch.mean((self.u_bc0 - u_bc0_pred) ** 2) + torch.mean((self.u_bc1 - u_bc1_pred) ** 2)
+
+            loss_pde = self.calc_pde_loss()
+
+            loss = loss_ic + loss_bc + loss_pde
             
-                
-                rcl, rcr = st.columns([3, 1])
+            self.ic_loss.append(loss_ic.item())
+            self.bc_loss.append(loss_bc.item())
+            self.pde_loss.append(loss_pde.item())
+            self.epoch_loss.append(loss.item())
 
-                with rcl:
-                    st.write("**Initial Condition**")
-                    st.markdown("**u(x, 0)** ")
-                    st.write("**Boundry Condition**")
+            loss.backward()
+            self.optimizer.step()
+            if epoch%100 == 0:
+                print(f"Epoch : {epoch}\nloss_ic : {loss_ic}\nloss_bc : {loss_bc}\npde_loss : {loss_pde}")
 
-                    st.markdown("**u(0, t)** ")
-                    st.markdown("**u(x, t)** ")
+    def predict(self, x, t):
+        self.model.eval()
+        with torch.no_grad():
+            predicted = self.model(torch.tensor(x).view(-1, 1), torch.tensor(t).view(-1, 1))
+        return predicted
+    
+    def inference(self):
+        self.x_test = torch.linspace(0, self.x_initial, self.linear_points).cpu()
+        self.t_test = torch.linspace(0, self.t_initial, self.linear_points).cpu()
 
-                    
-                with rcr:
-                    st.write("")
-                    st.write("")
-                    self.ic_func = st.text_input(
-                        "",
-                        value = "sin(x)",
-                        key = "ic",
-                        label_visibility = "collapsed"
-                    )
+        self.x_test, self.t_test = torch.meshgrid(self.x_test,  self.t_test)
 
-                    st.write("")
-                    st.write("")
-                    self.bc0 = st.number_input(
-                        "",
-                        value = 0,
-                        placeholder = 0.0,
-                        key = "bc0",
-                        label_visibility = "collapsed"
-                    )
+        self.model.eval()
+        self.model.cpu()
+        with torch.no_grad():
+            self.output = self.model(self.x_test.reshape(-1, 1), self.t_test.reshape(-1, 1))
 
-                    self.bc1 = st.number_input(
-                        "",
-                        value = 0, 
-                        placeholder = 0.0,
-                        key = "bc1",
-                        label_visibility = "collapsed"
-                    )
-            self.value_entered = st.form_submit_button("Submit")
-
-    def WaveEquation1D(self):
-        st.write("1D Wave Equation")
-
-
-    def getValues(self):
-        
-        return self.x, self.t, self.ic_func, self.bc0, self.bc1
+    
+    def plot_test_3d(self):
+        self.inference()
+        return self.x_test, self.t_test, self.output.reshape(self.linear_points, self.linear_points)
 
 
 
+    def plot_loss(self):
+        fig = plt.figure(figsize = (12, 6))
+        plt.plot(range(len(self.ic_loss)), self.ic_loss,label = "Initial Condition")
+        plt.plot(range(len(self.bc_loss)), self.bc_loss, label = "Boundry Condition")
+        plt.plot(range(len(self.pde_loss)), self.pde_loss, label = "PDE Loss")
+        plt.plot(range(len(self.epoch_loss)), self.epoch_loss, label = "Total Loss")
+        plt.legend()
+        return fig
 
--- mainPageFile.py
+++ mainPageFile.py
@@ -1,10 +1,14 @@
 import streamlit as st
+import streamlit.components.v1 as components
+import plotly.graph_objects as go
+
 import numpy as np
+import mpld3
 import torch
 import torch.nn as nn
 
 from HeatEquation1D import StreamlitPage
-from heat import PINN
+from HeatEquation1D import PINN
 
 
 class mainPage(StreamlitPage):
@@ -32,11 +36,59 @@ class mainPage(StreamlitPage):
             self.HE1D = PINN(self.x, self.t, self.ic0, self.bc0, self.bc1)
             self.page.value_entered = False
             self.HE1D.train()
+            self.plot_3d_chart()
 
-            st.pyplot(self.HE1D.plot_test_3d())
-            st.pyplot(self.HE1D.plot_loss())
-            
 
+    def plot_3d_chart(self):
+
+        X, Y, Z = self.HE1D.plot_test_3d()
+        surface = go.Surface(
+            z=Z,
+            x=X,
+            y=Y,
+            colorscale='Viridis',
+            opacity=0.97,
+            lighting=dict(ambient=0.6, diffuse=0.95, roughness=0.25, specular=0.1, fresnel=0.1),
+            lightposition=dict(x=80, y=120, z=40),
+            contours=dict(
+                z=dict(show=True, usecolormap = True, highlight = False, project_z = True),
+                x=dict(show=False, ),
+                y=dict(show=False, )
+            ),
+            showscale=True
+        )
+
+        camera = dict(
+            eye=dict(x=0.1, y=2.0, z=0.9),
+            up=dict(x=0, y=0, z=0),
+            center=dict(x=0, y=0, z=0)
+        )
+
+        layout = go.Layout(
+            title=dict(
+                text="1D Heat Equation",
+                x=0.2,
+                font=dict(size=22, family='Arial Black', color = 'black')
+            ),
+            scene=dict(
+                xaxis_title='Length',
+                yaxis_title='Time'
+                zaxis_title='Temperature',
+                xaxis=dict(showgrid=True, gridcolor='rgba(200,200,200,0.4)', backgroundcolor="rgba(250,250,250,1)"),
+                yaxis=dict(showgrid=True, gridcolor='rgba(200,200,200,0.4)', backgroundcolor="rgba(250,250,250,1)"),
+                zaxis=dict(showgrid=True, gridcolor='rgba(220,220,220,0.3)', backgroundcolor="rgba(255,255,255,1)"),
+                camera=camera,
+                aspectmode='manual',
+                aspectratio=dict(x=1, y=1, z=0.7)
+            ),
+            margin=dict(l=10, r=10, b=10, t=60),
+            paper_bgcolor="rgba(255,255,255,1)",
+            plot_bgcolor="rgba(245,245,245,0.6)",
+            font=dict(family="Segoe UI", size=13, color="#000000")
+        )
+
+        fig = go.Figure(data=[surface], layout=layout)
+        st.plotly_chart(fig, use_container_width=True)
 
 
 main = mainPage()
